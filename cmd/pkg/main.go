ba9f7902cbfb2265ae7749e8df4bd49c6243e120
Aditi Sinha <aditisinha.241cs202@nitk.edu.in>
2026-01-31 18:02:26 +0530

    added:worker

--- cmd/pkg/main.go
+++ cmd/pkg/main.go
@@ -15,10 +15,22 @@ import(
 func runTasks(w *worker.Worker){
 	for{
 		if w.Queue.Len()!=0{
+			t:=w.Queue.Peek().(*task.Task)
+			t.State=task.Scheduled
 			result:=w.RunTask()
 			if result.Error!=nil{
 				log.Printf("Error running tasks : %v\n",result.Error)
+				panic(result.Error)
 			}
+			fmt.Println(result)
+			fmt.Printf("Task %s is running in container %s\n",t.ContainerId,result.ContainerId)
+			fmt.Printf("Time to sleep\n")
+			time.Sleep(time.Second * 30)
+			res:=w.StopTask(t)
+			if res.Error!=nil{
+				panic(res.Error)
+			}
+			fmt.Printf("Stopped successfully %s\n",res.ContainerId)
 		}else{
 			log.Printf("No tasks to process currently \n")
 		}
@@ -31,19 +43,28 @@ func main(){
 	host:=os.Getenv("HOST")
 	port,_:=strconv.Atoi(os.Getenv("PORT"))
 
-	fmt.Println("Starting worker.....")
-
+	t:=task.Task{
+		ID:uuid.New(),
+		Name:"Task-1",
+		State:task.Pending,
+		Image:"nginx:latest",
+		Disk:2,
+	}
+	db:=make(map[uuid.UUID]*task.Task)
 	w:=worker.Worker{
 		Queue:*queue.New(),
-		Db:make(map[uuid.UUID]*task.Task),
+		Db:db,
 	}
+	fmt.Println("Starting worker.....")
+
+    w.AddTask(&t)
 
 	api:=worker.Api{
 		Address:host,
-		Worker:w,
+		Worker:&w,
 		Port:port,
 	}
 	go runTasks(&w)
-	go w.CollectStats()
+	// go w.CollectStats()
 	api.Start()
 }
